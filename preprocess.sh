0c9708212de4c44d0d4cb203883ec2cfad9156e1
Ubuntu <ubuntu@ip-172-31-18-151.us-west-2.compute.internal>
2020-05-24 13:44:06 -0400

    add small_old datasets and config

--- preprocess.sh
+++ preprocess.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-onmt_preprocess -config examples/learning_fix/config/small_preprocess_1G_1.yml
+#onmt_preprocess -config examples/learning_fix/config/small_preprocess_1G_1.yml
+onmt_preprocess -config examples/learning_fix/config/small_preprocess_1G_2.yml
--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-onmt_train -config examples/learning_fix/config/small_train_1G_1.yml
+#onmt_train -config examples/learning_fix/config/small_train_1G_1.yml
+onmt_train -config examples/learning_fix/config/small_train_1G_2.yml
