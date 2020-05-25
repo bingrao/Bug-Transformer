56fa6df27a5a221257a8c8e5a140cdb8aa698e64
Amose-Yao <amoseyao@gmail.com>
2020-05-25 00:55:59 -0400

    single gpu training

--- train_yao.sh
+++ train_yao.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-if [ "$#" -ne 1 ] ; then
+if [ "$#" -ne 2 ] ; then
   echo "Missing Parameters ..."
-  echo "Usage: $0 dataset[small|small_old|small_tree|small_path] " >&2
+  echo "Usage: $0 dataset[small|small_old|small_tree|small_path] CUDA_VISIBLE_DEVICES[0|1|2|3|just for single gpu] " >&2
   exit 1
 fi
 
 dataset=$1
 CurrentDate=$(date "+%Y-%m-%d-%H-%M-%S")
-ProjectLog=examples/learning_fix/log/${dataset}-${CurrentDate}.log
+ProjectLog=examples/learning_fix/log/yao/${dataset}-${CurrentDate}.log
+
+export CUDA_VISIBLE_DEVICES=$2
 
 case ${dataset} in
    "small")
       set -x
-      onmt_train -config examples/learning_fix/config/small_train_1G_1.yml
+      onmt_train -config examples/learning_fix/config/small_train_1G_1_yao_1.yml -world_size 1 -gpu_ranks 0 -log_file ${ProjectLog}
       ;;
    "small_old")
       set -x
-      onmt_train -config examples/learning_fix/config/small_train_1G_2.yml -log_file ${ProjectLog}
+      onmt_train -config examples/learning_fix/config/small_train_1G_2_yao_1.yml -world_size 1 -gpu_ranks 0 -log_file ${ProjectLog}
       ;;
    "small_tree")
       set -x
